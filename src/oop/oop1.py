5c5ff8009eb82ac0bf1eaf81ae1bf480cd9f6b98
Chris Cruz <christopherc1331@gmail.com>
2020-05-17 14:50:57 -0700

    Finished oop1.py problem

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,60 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# this is the base class
+# ======================
+class Vehicle:
+    def __init__(self, make, **kwargs):
+        self.make = make
+# ======================
+# this is the base class
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self, wing_type, **kwargs):
+        super().__init__(kwargs)
+        self.wing_type = wing_type
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, tire_type, **kwargs):
+        super().__init__(kwargs)
+        self.tire_type = tire_type
+
+
+class Car(GroundVehicle):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.type = "car"
+
+    def __str__(self):
+        return f"The {self.make} {self.type} has {self.tire_type} wheels and can be seen stuck in traffic"
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.type = "motorcycle"
+
+    def __str__(self):
+        return f"The {self.make} {self.type} has {self.tire_type} wheels and can be seen splitting lanes"
+
+
+class Airplane(FlightVehicle):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.type = "airplane"
+
+    def __str__(self):
+        return f"The {self.make} {self.type} has {self.wing_type} wings and can be seen soaring through the skies"
+
+
+class Starship(FlightVehicle):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.type = "starship"
+
+    def __str__(self):
+        return f"The {self.make} {self.type} has {self.wing_type} wings and can be seen bending space and time"
